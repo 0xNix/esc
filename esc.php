e6ad5119c87ad4f70693e38cf131eb1833562e9c
DenisHys <43570272+0xNix@users.noreply.github.com>
2022-03-05 19:51:31 +0700

    Add files via upload

---
+++ esc.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Hacked By 0xNix</title>
+</head>
+<body background="http://4.bp.blogspot.com/-h1QH3MmXd4M/Uc64vsqmPeI/AAAAAAAAALs/Gi-fPnZ5V2Q/s1600/black+(11).jpg">
+<body>
+<script>
+    </script>
+</style>
+ 
+<center><br><img src="https://image.ibb.co/hWAnfA/IMG-20181110-015020.jpg" width="400" height="350"><center>
+<body>
+ 
+<font face= "Architects Daughter">
+<font color='red' face='Architects Daughter' font size="5px">Explosion Squad Cyber</font></b><center>
+<?php
+echo '<big><font color=white> System: <span style="color: lime;">'.php_uname().'</span></big><br>';
+echo "<font color=white>Your IP: <font color=blue>".$_SERVER['REMOTE_ADDR']."</font><br>";
+<a href='https://www.facebook.com/EscTeam2018/' target='_blank'><font size='3px'>About Us</a> -
+<a href='https://explosionsquad-cyber.blogspot.com/' target='_blank'><font size='3px'>Website</a>  -
+</body>
+</html>
