647082c0a72c5125f9532976cdb1036bcd084370
DenisHys <43570272+0xNix@users.noreply.github.com>
2022-05-27 11:12:59 -0400

    esc.php

--- esc.php
+++ esc.php
@@ -1,29 +1,19 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title>Hacked by 0xNix</title>
-</head>
-<body background="http://4.bp.blogspot.com/-h1QH3MmXd4M/Uc64vsqmPeI/AAAAAAAAALs/Gi-fPnZ5V2Q/s1600/black+(11).jpg">
-<body>
-<script>
-    </script>
-</style>
- 
-<center><br><img src="https://image.ibb.co/hWAnfA/IMG-20181110-015020.jpg" width="400" height="350"><center>
-<body>
- 
-<font face= "Architects Daughter">
-<font color='red' face='Architects Daughter' font size="5px"'>Hacked by 0xNix | ESC Team</font></b><center>
-<?php
-echo '<big><font color=white> System: <span style="color: lime;">'.php_uname().'</span></big><br>';
-echo "<font color=white>Your IP: <font color=blue>".$_SERVER['REMOTE_ADDR']."</font><br>";
-?>
-</body>
-<br>
-<br>
-<br>
-<style> .error { text-align: center; font-family: 'Gilda Display', serif; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; overflow: default; } .info { text-align: center; width: 200px; height: 60px; margin: auto; position: absolute; top: 280px; bottom: 0; left: 20px; right: 0; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; } .info:after { content: 'OWNED'; font-family: OCR-A; font-size: 100px; text-align: center; width: 800px; margin: auto; position: absolute; top: 20px; bottom: 0; left: 40px; right: 0; opacity: 0; color: white; -webkit-animation: noise-1 .2s linear infinite; animation: noise-1 .2s linear infinite; } @-webkit-keyframes noise-1 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: -6px;} 80% {opacity: .3;} 100% {opacity: .6; left: 2px;} } @keyframes noise-1 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: -6px;} 80% {opacity: .3;} 100% {opacity: .6; left: 2px;} } @-webkit-keyframes noise-2 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: 6px;} 80% {opacity: .3;} 100% {opacity: .6; left: -2px;} } @keyframes noise-2 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: 6px;} 80% {opacity: .3;} 100% {opacity: .6; left: -2px;} } @-webkit-keyframes noise { 0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);} 43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);} } @keyframes noise { 0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);} 43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);} } @-webkit-keyframes noise-3 { 0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);} 43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate (60deg);} } @keyframes noise-3 { 0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);} 43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate (60deg);} } .wrap { top: 30%; left: 25%; height: 200px; margin-top: -100px; position: absolute; } code { color: white; } span.blue { color: #48beef; } span.comment { color: #7f8c8d; } span.orange { color: #f39c12; } span.green { color: #33cc33; } .viewFull { font-family:OCR-A; color:orange; text-decoration:; }    } @media only screen and (min-height: 500px) { .viewFull{ display:none;        } }     </style> <center> <style> img { filter:alpha(opacity=70); -moz-opacity:0.7; opacity:0.7; } img:hover { filter:alpha(opacity=100); -moz-opacity:.0; opacity:1.0; } </style>
-<a href='https://www.facebook.com/EscTeam2018/' target='_blank'><font size='3px'>- About Us -</a>
-</body>
-</html>
+
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml"><head>
+<link rel="icon" type="image/png" href="http://www.zone-h.com/images/cflags/png/id.png"/>
+<title>Hacked By 0xNix</title>
+<script src="/_private/static/script.min.js"></script><script>run('46a313c574d7e3ac-0fa1aac09342465dbab12a70-fa639f5bf34f5593c6daaa02f5574122b5d8b901648284e66f3c2288d3f5', 'js_session1', false);</script></head><body style="background-color: rgb(0, 0, 0); color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b"><br/><br/><br/>
+
+<div style="text-align: center;">
+<span style="color: rgb(127, 127, 126);"><br/>
+<span style="font-family: monospace;">
+ <img border="0" src="http://zonehmirrors.org/defaced/2015/05/17/idca.or.id/zonehmirrors.org/defaced/2014/12/02/banten-cyber.web.id/upload.wikimedia.org/wikipedia/commons/7/7c/Devil_cartoon_charactor.gif" width="195" height="314"/><br/>
+ <span style="color: rgb(253, 253, 251);">Owned by 0xNix<br/><br/>	
+<font color="red">Foursdeath Team - Explosion Squad Cyber<br/></font>
+</span></span></span><span style="color: rgb(127, 127, 126);"><span style="font-family: monospace;"></span></span><span style="color: rgb(255, 255, 255);">
+<br style="font-family: monospace; color: rgb(127, 127, 126);"/>
+<span style="font-family: monospace; color: rgb(127, 127, 126);"><span style="color: rgb(253, 253, 251);">
+<a target="blank" href="https://www.facebook.com/Fsdth2018Team/">Touch Me</a><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<span style="font-family: monospace; color: rgb(127, 127, 126);"></span><span style="color: rgb(253, 253, 251);"></span></span></span></span></div>
+
+</body></html><!-- end: index -->
